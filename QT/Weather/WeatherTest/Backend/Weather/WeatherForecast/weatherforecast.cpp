d0ff74fabda03435e147f8237ccad527521ef00c
AP02 <none>
2016-03-28 15:45:07 +0200

    Start implementierung Weatherforcast

--- QT/Weather/WeatherTest/Backend/Weather/WeatherForecast/weatherforecast.cpp
+++ QT/Weather/WeatherTest/Backend/Weather/WeatherForecast/weatherforecast.cpp
@@ -1,4 +1,5 @@
 #include "weatherforecast.h"
+#include "patternhandler.h"
 
 WeatherForecast::WeatherForecast(QObject *parent) : Search(parent)
 {
@@ -7,5 +8,7 @@ WeatherForecast::WeatherForecast(QObject *parent) : Search(parent)
 
 Search::FilterStatus_t WeatherForecast::filterData(QString webData)
 {
+    Pattern *windPattern, *tempPattern, *rainPattern, *sunPattern;
+    windPattern = PatternHandler::getInstance()->getPattern(PatternHandler::WIND)
 
 }
