82845932032c380003638ea71288b2a6dbfeb6ef
SeidC <SeidelChristof@googlemail.com>
2016-07-24 23:21:34 +0200

    Implementation of IO Test

--- AVR/GccApplication1/GccApplication1/main.cpp
+++ AVR/GccApplication1/GccApplication1/main.cpp
@@ -19,23 +19,12 @@ int main(void)
     BaseTypes::BitStatus_e stat;
     ioPortB.setDirection(Io::BIT_0,Io::OUTPUT);
     ioPortA.setDirection(Io::BIT_0,Io::INPUT);
-  
-      
-      
-      
-    
-      //((value & (bit+1)) >> bit )
-    
+ 
     /* Replace with your application code */
     while (1) 
     {
-       
-        
         stat = ioPortA.getPin(Pin::PIN_0);
-        if(stat == BaseTypes::BIT_HIGH)
-        {
-            ioPortB.setPin(Port::PIN_0,Port::PIN_HIGH);
-        }        
+        ioPortB.setPin(Port::PIN_0,(Port::PinStatus_e)stat);
     }
 }
 
